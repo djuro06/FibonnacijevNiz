a580edd362a53dd03daf02522846c799bd786bf8
Marko Đuroković <djuro06@gmail.com>
2017-06-10 20:16:15 +0200

    Add files via upload

---
+++ fibonaccijevNiz.php
@@ -0,0 +1,27 @@
+<?php
+
+
+// zadana varijabla
+$v=isset($_GET["v"]) ? $_GET["v"] : "";
+$v++;
+
+// vrijednost polja
+$x = array(-1, 1);
+
+// sljedeća vrijednost polja
+$x[2] = $x[0] + $x[1];
+
+// petlja koja ispisuje fibonaccijev niz
+for ($y = 2; $y <= $v; ++$y) {
+	$x[$y] = $x[$y - 1] + $x[$y - 2];
+	echo $x[$y];
+	echo "<br />";
+}
+echo "<br />";
+
+// prvojera je li uneseni broj veći od 2
+if ($v = $_GET["v"] <= 2){
+	echo "Vrijednost mora biti veća od 2";
+} else {
+	echo "Vrijednost fibonaccijevog niza je " . $x[$y-1] / $x[$y-2];	 
+}
