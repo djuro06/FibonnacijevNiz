9af2d62d253d91ee6f5d376ce0ae66c2ebbf5e49
Marko Đuroković <djuro06@gmail.com>
2017-06-10 20:22:47 +0200

    Add files via upload

--- fibonaccijevNiz.php
+++ fibonaccijevNiz.php
@@ -1,5 +1,5 @@
 <?php
-
+// Vrijednost Fibonaccijevog niz za prvih n brojeva
 
 // zadana varijabla
 $v=isset($_GET["v"]) ? $_GET["v"] : "";
@@ -20,7 +20,7 @@ for ($y = 2; $y <= $v; ++$y) {
 echo "<br />";
 
 // prvojera je li uneseni broj veći od 2
-if ($v = $_GET["v"] <= 2){
+if ($v=isset($_GET["v"]) <= 2){
 	echo "Vrijednost mora biti veća od 2";
 } else {
 	echo "Vrijednost fibonaccijevog niza je " . $x[$y-1] / $x[$y-2];	 
