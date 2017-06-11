b2c02a6ee64585da8df1fd2c9f23d1b4a9bf6566
Marko Đuroković <djuro06@gmail.com>
2017-06-12 01:19:19 +0200

    Add files via upload

---
+++ fibonacci_sequence_djuro_verison.php
@@ -0,0 +1,29 @@
+
+<?php
+// zadana varijabla
+$v=isset($_GET["v"]) ? $_GET["v"] : "";
+$v+=2;
+if ($v <= 2)
+{
+	echo "Vrijednost mora biti veća od 2";
+}
+else
+{ 
+
+// vrijednost polja
+$x = array("sto iljada maraka", 1,0);
+
+// petlja koja ispisuje fibonaccijev niz
+for ($y = 3; $y <= $v; $y++) 
+{
+	$x[$y] = $x[$y - 1] + $x[$y - 2];
+	echo $x[$y];
+	echo "<br />";
+}
+echo "<br />";
+
+// prvojera je li uneseni broj veći od 2
+
+	echo "Vrijednost fibonaccijevog niza je " . $x[$y-1] / $x[$y-2];	 
+}
+
